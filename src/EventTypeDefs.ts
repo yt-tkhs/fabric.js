449f6e0104feb10f96d3ee809b49c57d1cea7d9c
ShaMan123 <shacharnen@gmail.com>
2023-12-09 05:05:56 +0200

    rename

--- src/EventTypeDefs.ts
+++ src/EventTypeDefs.ts
@@ -104,7 +104,7 @@ export type TModificationEvents =
   | 'rotating'
   | 'skewing'
   | 'resizing'
-  | 'modifyPoly';
+  | 'polyChanged';
 
 export interface ModifiedEvent<E extends Event = TPointerEvent>
   extends TEvent<E> {
